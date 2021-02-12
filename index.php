4a0c8fd0f94168346842227da820a86667af7a17
Modou KEBE <78542442+modoukebe@users.noreply.github.com>
2021-02-12 10:01:41 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tous les programmes du cycle secodaire</title>
+	<link rel="stylesheet" href="Academica.css"/>
+</head>
+<body>
+     <header>
+     	 <p> welcom to the academi's of KEBE </p>
+     	 <img src="TH.jpg" alt="Enseigner" title="éducation"  id="l1"/>
+     	 <img src="repere.jpg" alt="Enseigner" title="éducation"  id="l4"/>
+     	 <img src="thales.jpg" alt="Enseigner" title="éducation"  id="l2"/>
+	 </header>
+      <nav>
+      	<div>
+	   <ul>
+	    <li>
+	    	<a href="Accueil.php">Accueil</a>
+	    </li>
+	    <li>
+	    	<a href="Cours.php">Cours</a>
+	    </li>
+	    <li> 
+	    	<a href="Exercices.php">Exercices</a>
+	    </li>
+        <li><a href="Controles.php">Controles</a></li>
+	  </ul>
+	  </div>
+	</nav>
+	</body>
+</html>
